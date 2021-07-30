7b3c8667715af3944df3f8ffa400d061e8998879
Code Sandbox <hello@codesandbox.io>
2021-07-30 18:22:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6fad64332df5fe13829f754c6a530cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6291bf73cf6e9036668dec3069fb16",
+    "url": "https://jayrath17.github.io/csb-olczd/index.html"
+  },
+  {
+    "revision": "61e601124626580786f0",
+    "url": "https://jayrath17.github.io/csb-olczd/static/css/main.3ea1a502.chunk.css"
+  },
+  {
+    "revision": "7d05e369ea1da67d3a57",
+    "url": "https://jayrath17.github.io/csb-olczd/static/js/2.4c9c3bd5.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "https://jayrath17.github.io/csb-olczd/static/js/2.4c9c3bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e601124626580786f0",
+    "url": "https://jayrath17.github.io/csb-olczd/static/js/main.35bff3ff.chunk.js"
+  },
+  {
+    "revision": "9384f8b24cc5d96d0ca3",
+    "url": "https://jayrath17.github.io/csb-olczd/static/js/runtime-main.57550fe2.js"
+  }
+]);
